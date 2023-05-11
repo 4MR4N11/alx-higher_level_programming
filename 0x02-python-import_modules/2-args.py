b372f91c228624891778981e56fa3c393be7daf3
4mr4n11 <amraniikhalid0606@gmail.com>
2023-05-11 16:24:31 +0100

    Mandatory part

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    args_num = len(argv)
+    i = 1
+    if args_num == 1:
+        print("{} arguments.".format(args_num - 1))
+    else:
+        print("{} arguments:".format(args_num - 1))
+        while i < args_num:
+            print("{}: {}".format(i, argv[i]))
+            i += 1
