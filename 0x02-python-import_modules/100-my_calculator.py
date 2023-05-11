01ec14d1f01c7ba12f82bd92af860911eab11d75
4mr4n11 <amraniikhalid0606@gmail.com>
2023-05-11 17:27:51 +0100

    Advanced part

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import div, sub, mul, add
+    from sys import argv
+    args_num = len(argv)
+    if args_num != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    op = argv[2]
+    a = int(argv[1])
+    b = int(argv[3])
+    if op != "/" and op != "+" and op != "-" and op != "*":
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    print("{:d} {:s} {:d} = ".format(a, op, b), end='')
+    if op == "+":
+        print("{}".format(a + b))
+    elif op == "-":
+        print("{}".format(a - b))
+    elif op == "/":
+        print("{}".format(a / b))
+    else:
+        print("{}".format(a * b))
