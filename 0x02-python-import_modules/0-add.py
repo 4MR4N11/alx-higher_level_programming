9569f9c6f7cd74b798ef003238395180d2bbd899
4mr4n11 <amraniikhalid0606@gmail.com>
2023-05-11 15:48:49 +0100

    Mandatory part

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-from add_0 import add
-a = 1
-b = 2
-result = add(a, b)
-print("{a} + {b} = {result}".format(a=a, b=b, result=result))
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{:s} + {:s} = {:s}".format(str(a), str(b), str(add(a, b))))
