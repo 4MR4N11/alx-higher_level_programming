7db7bdcd16513215f3a99669b1d84124bbd78280
4mr4n11 <amraniikhalid0606@gmail.com>
2023-05-11 16:26:49 +0100

    Mandatory part

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,9 @@ if __name__ == "__main__":
     i = 1
     if args_num == 1:
         print("{} arguments.".format(args_num - 1))
+    elif args_num == 2:
+        print("{} argument:".format(args_num - 1))
+        print("{}: {}".format(i, argv[i]))
     else:
         print("{} arguments:".format(args_num - 1))
         while i < args_num:
