50ed6bd2a80e8446c78ba65ded3f905ded764940
4mr4n11 <amraniikhalid0606@gmail.com>
2023-05-13 22:17:33 +0100

    Mandatory part

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,14 +4,14 @@ def add_tuple(tuple_a=(), tuple_b=()):
     tmp1 = 0
     tmp2 = 0
     i = 0
-    while i < len(tuple_a):
+    while i < len(tuple_a) and i < 2:
         if i == 0:
             tmp1 += tuple_a[i]
         else:
             tmp2 += tuple_a[i]
         i += 1
     i = 0
-    while i < len(tuple_b):
+    while i < len(tuple_b) and i < 2:
         if i == 0:
             tmp1 += tuple_b[i]
         else:
