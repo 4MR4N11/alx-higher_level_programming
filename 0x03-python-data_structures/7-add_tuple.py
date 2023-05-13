bb06cd04eb3ab00615bd5e1da9fef885b7db8505
4mr4n11 <amraniikhalid0606@gmail.com>
2023-05-13 22:16:45 +0100

    Mandatory part

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    new_tuple = ()
+    tmp1 = 0
+    tmp2 = 0
+    i = 0
+    while i < len(tuple_a):
+        if i == 0:
+            tmp1 += tuple_a[i]
+        else:
+            tmp2 += tuple_a[i]
+        i += 1
+    i = 0
+    while i < len(tuple_b):
+        if i == 0:
+            tmp1 += tuple_b[i]
+        else:
+            tmp2 += tuple_b[i]
+        i += 1
+    new_tuple = (tmp1, tmp2)
+    return new_tuple
