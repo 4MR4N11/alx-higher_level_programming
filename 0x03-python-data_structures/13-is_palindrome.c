7bb364e190576e8a654d59f60d517ef2ad1a0d00
4mr4n11 <amraniikhalid0606@gmail.com>
2023-05-13 23:40:45 +0100

    Mandatory part

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,63 +1,30 @@
 #include "lists.h"
 
 /**
- * get_list_len - get the length of a linked list
- * @h: pointer to head of list
- * Return: length of list
- */
-
-int get_list_len(listint_t *h)
-{
-	int size;
-
-	size = 0;
-	while (h)
-	{
-		size++;
-		h = h->next;
-	}
-	return (size);
-}
-
-/**
  * is_palindrome - checks if a singly linked list is a palindrome
  * @head: pointer to head of list
- * Return: 1 if palindrome, 0 if not
+ * Return: 0 if not a palindrome, 1 if it is a palindrome
  */
 
 int is_palindrome(listint_t **head)
 {
-	int *half_list;
 	listint_t *tmp;
-	int half_len, list_len, i = 0;
+	int stack[1000], top = -1;
 
 	if (!head || !*head || !(*head)->next)
 		return (1);
 	tmp = *head;
-	list_len = get_list_len(tmp);
-	half_len = list_len / 2;
-	half_list = malloc(sizeof(int) * half_len);
-	if (!half_list)
-		return (0);
-	while (tmp && i < half_len)
+	while (tmp)
 	{
-		half_list[i] = tmp->n;
+		stack[++top] = tmp->n;
 		tmp = tmp->next;
-		i++;
 	}
-	if (list_len % 2 != 0)
-		tmp = tmp->next;
-	i = half_len - 1;
-	while (tmp && i >= 0)
+	tmp = *head;
+	while (tmp)
 	{
-		if (tmp->n != half_list[i])
-		{
-			free(half_list);
+		if (tmp->n != stack[top--])
 			return (0);
-		}
-		i--;
 		tmp = tmp->next;
 	}
-	free(half_list);
 	return (1);
 }
