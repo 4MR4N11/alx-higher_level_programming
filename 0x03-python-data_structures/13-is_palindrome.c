326cd0ce64cd7baf3e11486c611c3488366c5a40
4mr4n11 <amraniikhalid0606@gmail.com>
2023-05-14 00:09:04 +0100

    Mandatory part

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,30 +1,49 @@
 #include "lists.h"
 
 /**
+ * reverse_list - reverses a listint_t linked list
+ * @head: pointer to head of list
+ * Return: pointer to first node of reversed list
+ */
+
+listint_t *reverse_list(listint_t *head)
+{
+	listint_t *prev = NULL, *current = head, *next = NULL;
+
+	while (current)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+	return (prev);
+}
+
+/**
  * is_palindrome - checks if a singly linked list is a palindrome
  * @head: pointer to head of list
- * Return: 0 if not a palindrome, 1 if it is a palindrome
+ * Return: 1 if palindrome, 0 if not
  */
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *tmp;
-	int stack[1000], top = -1;
+	listint_t *copy_head = NULL, *tmp = *head;
 
-	if (!head || !*head || !(*head)->next)
-		return (1);
-	tmp = *head;
 	while (tmp)
 	{
-		stack[++top] = tmp->n;
+		add_nodeint_end(&copy_head, tmp->n);
 		tmp = tmp->next;
 	}
 	tmp = *head;
-	while (tmp)
+	copy_head = reverse_list(copy_head);
+	while (tmp && copy_head)
 	{
-		if (tmp->n != stack[top--])
+		if (tmp->n != copy_head->n)
 			return (0);
 		tmp = tmp->next;
+		copy_head = copy_head->next;
 	}
+	free_listint(copy_head);
 	return (1);
 }
