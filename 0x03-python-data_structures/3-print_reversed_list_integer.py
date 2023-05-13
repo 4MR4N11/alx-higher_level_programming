21879106a2ca4d1fc744249d0175ccd48501e220
4mr4n11 <amraniikhalid0606@gmail.com>
2023-05-13 20:59:50 +0100

    Mandatory part

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    if not my_list:
+        return
     copy_list = my_list.copy()
     copy_list.reverse()
     for i in copy_list:
