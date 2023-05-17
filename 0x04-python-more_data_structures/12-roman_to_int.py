8e0df88dcc8f56a3d957e8ccb84d6b9030478a30
4mr4n11 <amraniikhalid0606@gmail.com>
2023-05-17 15:39:37 +0100

    Mandatory part

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,21 +3,14 @@ def roman_to_int(roman_string):
     if roman_string and isinstance(roman_string, str):
         num = 0
         i = 0
-        tmp = ''
         length = len(roman_string)
-        dic = {"IV": 4, "IX": 9, "XL": 40, "LC": 90, "CD": 400, "CM": 900,
-               'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         while i < length:
-            if i + 1 < length:
-                tmp = roman_string[i]+roman_string[i+1]
-            for key, value in dic.items():
-                if key == tmp:
-                    num += value
-                    i += 1
-                    break
-                elif key == roman_string[i]:
-                    num += value
-                    tmp = ""
-            i += 1
+            if i+1 < length and dic[roman_string[i]] < dic[roman_string[i+1]]:
+                num += dic[roman_string[i + 1]] - dic[roman_string[i]]
+                i += 2
+            else:
+                num += dic[roman_string[i]]
+                i += 1
         return num
     return 0
