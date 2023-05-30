e9a42624c4cada7fa25ecfb2fb5a1c6aae73aabd
4mr4n11 <amraniikhalid0606@gmail.com>
2023-05-30 18:56:57 +0100

    Mandatory part

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -80,7 +80,7 @@ class Square():
             print()
         else:
             for i in range(0, self.__position[1]):
-                print(" ")
+                print("")
             for i in range(0, self.__size):
                 for j in range(0, self.__position[0]):
                     print(" ", end="")
