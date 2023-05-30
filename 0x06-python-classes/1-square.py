b34ec8e5ad56bf37f5e6569dc4b93634082e4992
4mr4n11 <amraniikhalid0606@gmail.com>
2023-05-30 17:57:58 +0100

    Mandatory part

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,9 +4,11 @@
 
 class Square():
     """Square class with private instance attribute size"""
-    def __init__(self, size):
-        """__init__ method
+    def __init__(self, size=0):
+        """__init__ method.
+
         Args:
             size (int): size of square
+
         """
         self._size = size
