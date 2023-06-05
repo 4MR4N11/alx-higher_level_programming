06eb12c349eb48ac0ee724bf3ab03ade3a51d078
4mr4n11 <amraniikhalid0606@gmail.com>
2023-06-05 14:47:09 +0100

    Mandatory part

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -28,7 +28,7 @@ class Rectangle:
             value (int): width
         """
         self.__width = value
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -48,7 +48,7 @@ class Rectangle:
             value (int): height
         """
         self.__height = value
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
