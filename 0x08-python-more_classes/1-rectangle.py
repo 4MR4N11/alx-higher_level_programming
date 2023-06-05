ec4385c1e296a212c2dd5f433752e643dc8f0c5c
4mr4n11 <amraniikhalid0606@gmail.com>
2023-06-05 14:38:52 +0100

    Mandatory part

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""rectangle module"""
+
+
+class Rectangle:
+    """rectangle class"""
+    def __init__(self, width=0, height=0):
+        """initializes the rectangle class
+        Args:
+            width: width of rectangle
+            height: height of rectangle
+        """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """width getter
+        Return:
+            int: width
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """width setter
+        Args:
+            value (int): width
+        """
+        self.__width = value
+        if type(self.__width) is not int:
+            raise TypeError("width must be an integer")
+        if self.__width < 0:
+            raise ValueError("width must be >= 0")
+
+    @property
+    def height(self):
+        """height getter
+        Return:
+            int: height
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height setter
+        Args:
+            value (int): height
+        """
+        self.__height = value
+        if type(self.__height) is not int:
+            raise TypeError("height must be an integer")
+        if self.__height < 0:
+            raise ValueError("height must be >= 0")
