2202dd036f23a1ac4f0e0b4ec8785f7208f8577b
4mr4n11 <amraniikhalid0606@gmail.com>
2023-06-19 17:12:15 +0100

    Mandatory part

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -9,14 +9,26 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """class constructor"""
         super().__init__(id)
-        self.__width = None
-        self.__height = None
-        self.__x = None
-        self.__y = None
-        self.height = height
-        self.width = width
-        self.x = x
-        self.y = y
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
+        if type(x) is not int:
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
+        if type(y) is not int:
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
+        self.__height = height
+        self.__width = width
+        self.__x = x
+        self.__y = y
 
     @property
     def height(self):
