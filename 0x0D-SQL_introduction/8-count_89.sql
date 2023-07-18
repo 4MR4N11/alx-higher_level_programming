612504c0ea91b326245073c455f4c8bee67e8111
4mr4n11 <amraniikhalid0606@gmail.com>
2023-07-18 15:37:04 +0100

    Mandatory part

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,4 +1,4 @@
 --This script displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in your MySQL server.
-SELECT COUNT(id) AS number_of_records
+SELECT COUNT(*) AS number_of_records
 FROM first_table
 WHERE id = 89;
---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,16 @@
+-- This script creates a table second_table in the current database in your MySQL server.
+-- second_table description:
+-- id INT
+-- name VARCHAR(256)
+-- score INT
+-- If the table second_table already exists, your script should not fail
+CREATE TABLE IF NOT EXISTS second_table(
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
+INSERT INTO second_table (id, name, score) VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
