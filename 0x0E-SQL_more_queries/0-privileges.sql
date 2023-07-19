f11b87d161c43623f17019279ae41358b4411de6
4mr4n11 <amraniikhalid0606@gmail.com>
2023-07-19 12:56:53 +0100

    Mandatory part

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- all privileges of the MySQL users user_0d_1 and user_0d_2
+-- This script lists all privileges of the MySQL users user_0d_1 and user_0d_2
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
