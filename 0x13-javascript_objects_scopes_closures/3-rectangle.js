d9c46f28448672828ddd32ac20a8810f6a796afd
Khalid El Amrani <amraniikhalid0606@gmail.com>
2023-08-15 22:50:04 +0100

    Mandatory part

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -2,11 +2,10 @@
 
 class Rectangle {
   constructor (w, h) {
-    if ((!w || w < 0) || (!h || h < 0)) {
-      return {};
-    }
-    this.width = w;
-    this.height = h;
+    if ((w > 0) && (h > 0)) {
+		this.width = w;
+		this.height = h;
+	}
   }
 
   print () {
