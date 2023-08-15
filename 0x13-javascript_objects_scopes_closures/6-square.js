1747751c571ac62ffdbfdc9369ef743bdad98a19
Khalid El Amrani <amraniikhalid0606@gmail.com>
2023-08-15 22:53:23 +0100

    Mandatory part

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,21 +3,16 @@
 const parentSquare = require('./5-square');
 
 class Square extends parentSquare {
-  constructor (size) {
-    super(size);
-    this.size = size;
-  }
-
   charPrint (c) {
     let char = 'X';
     if (c) {
       char = char.replace('X', 'C');
     }
     let line = '';
-    for (let i = 0; i < this.size; i++) {
+    for (let i = 0; i < this.width; i++) {
       line += char;
     }
-    for (let j = 0; j < this.size; j++) {
+    for (let j = 0; j < this.height; j++) {
       console.log(line);
     }
   }
