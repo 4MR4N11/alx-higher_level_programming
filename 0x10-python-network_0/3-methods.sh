bdcba41060a8710e297527fb46a048620f7ced75
Khalid El Amrani <amraniikhalid0606@gmail.com>
2023-08-31 13:18:45 +0100

    Mandatory part

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sI "$1" | grep "Allow" | awk '{print $2}' | sed 's/,/, /g'
+curl -sI "$1" | grep "Allow" | cut -d " " -f2-
