1f70c180d895aa8b28391a41720b6f425b407616
Khalid El Amrani <amraniikhalid0606@gmail.com>
2023-09-01 15:39:47 +0100

    Mandatory part

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-'''Python script that fetches https://intranet.hbtn.io/status'''
+'''Python script that fetches https://alx-intranet.hbtn.io/status'''
 import requests
 
 
 if __name__ == "__main__":
-    resp = requests.get('https://intranet.hbtn.io/status')
+    resp = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
     print("\t- type: {}".format(type(resp.text)))
     print("\t- content: {}".format(resp.text))
